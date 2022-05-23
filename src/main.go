90ce72c154ef765fc40553fb13844d0dfe34e0a0
Jader Campos <jader.campos@gmail.com>
2022-05-22 22:03:53 -0300

    Criando interface

--- src/main.go
+++ src/main.go
@@ -7,16 +7,28 @@ import (
 	"estudogolang.com/src/contas"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaDoBruno := contas.ContaCorrente{Titular: clientes.Titular{
 		Nome:      "Bruno",
 		CPF:       "123.111.123.12",
 		Profissao: "Desenvolvedor"},
 		NumeroAgencia: 123, NumeroConta: 123456, Operacao: 1}
+	contaDoBruno.Depositar(5000)
+	PagarBoleto(&contaDoBruno, 500)
 	fmt.Println(contaDoBruno)
 
 	clienteSilvia := clientes.Titular{"Silvia", "321.546.789.44", "Puta"}
 	contaDaSilvia := contas.ContaPoupanca{Titular: clienteSilvia, NumeroAgencia: 456, NumeroConta: 789789, Operacao: 13}
+	contaDaSilvia.Depositar(2000)
+	PagarBoleto(&contaDaSilvia, 500)
 	fmt.Println(contaDaSilvia)
 
 }
