d6931a8c1dc2a82c809f360c4ac6d763af8bbddb
Jader Campos <jader.campos@gmail.com>
2022-05-22 21:54:58 -0300

    Conta corrente e poupança sem interface

--- src/main.go
+++ src/main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 
-	"estudogolang.com/src/pessoa"
+	"estudogolang.com/src/clientes"
+	"estudogolang.com/src/contas"
 )
 
 func main() {
-	p1 := pessoa.Pessoa{Nome: "João"}
-	fmt.Println("Olá", p1.Nome)
+	contaDoBruno := contas.ContaCorrente{Titular: clientes.Titular{
+		Nome:      "Bruno",
+		CPF:       "123.111.123.12",
+		Profissao: "Desenvolvedor"},
+		NumeroAgencia: 123, NumeroConta: 123456}
+	fmt.Println(contaDoBruno)
+
+	clienteSilvia := clientes.Titular{"Silvia", "321.546.789.44", "Puta"}
+	contaDaSilvia := contas.ContaCorrente{Titular: clienteSilvia, NumeroAgencia: 456, NumeroConta: 789789}
+	fmt.Println(contaDaSilvia)
+
 }
--- src/pessoa/pessoa.go
+++
@@ -1,5 +0,0 @@
-package pessoa
-
-type Pessoa struct {
-	Nome string
-}
